229f710a226a9142986d69e806380b63846ad5a8
Stefano Garzarella <sgarzare@redhat.com>
2021-09-30 12:38:49 +0200

    vm-firstboot-base.sh: disable selinux to avoid issues

--- vm-firstboot-base.sh
+++ vm-firstboot-base.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -x
 
+ENFORCE=$(getenforce)
+
+# rpm fails if it runs from init
+# (error: failed to exec scriptlet interpreter /bin/sh: Permission denied)
+setenforce Permissive
+
 # Waiting network is up
 sleep 10
 
@@ -8,5 +14,7 @@ dnf -y --best update
 
 dnf -y install @virtualization libguestfs-tools libvirt libvirt-nss nfs-utils \
     lksctp-tools tuned grubby rsync gperftools fio perf gdb liburing
+
+setenforce $ENFORCE
 
 poweroff
