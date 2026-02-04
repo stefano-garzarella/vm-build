227a91d217e95ecefbe1acef773c1c3e3e6c194b
Stefano Garzarella <sgarzare@redhat.com>
2026-02-04 15:37:20 +0100

    Update to Fedora 42 default base image

--- vm-firstboot-base.sh
+++ vm-firstboot-base.sh
@@ -15,7 +15,7 @@ dnf -y --best update
 dnf -y install @virtualization libguestfs-tools libvirt libvirt-nss nfs-utils \
     lksctp-tools tuned grubby rsync gperftools fio perf gdb liburing driverctl \
     nmap git meson python3-docutils rust cargo rustfmt clippy diffutils \
-    alsa-utils
+    alsa-utils socat vng
 
 setenforce $ENFORCE
 
